5e5d13254fed617fa29accfb98cd5478dd5dd443
Ravi Arora <aroraravi87@live.com>
2020-12-22 21:57:44 +0000

    Commit startup file

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# print("Hello World")
+
+# Name = input("Enter your Name \n")
+# lang = input("Enter your fav Language \n")
+
+# print(f"welcome {Name}, you first program is successful in {lang}")
+
+
+# is_approved=True
+# is_valid_user=False
+
+# if is_approved and is_valid_user:
+# 	print("welcome in phython world");
+# else :
+# 	print("Contact to administrator");
+
+# is_valid_user=True if is_approved else False
+
+
+# if is_valid_user :
+# 	print("welcome in phython world");
+# else :
+# 	print("Contact to administrator");
+
+	#list
+# collection=[1,2,3,4,5,6,7,8,9,10]
+# print(collection[0::2])
+
+#dict
+dictonery={
+'1':'welcome',
+'2':'hello world',
+'3':'sample'
+}
+
+print(dictonery['1']);
+print(dictonery['2']);
+print(dictonery['3']);
