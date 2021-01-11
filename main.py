d98cacec0960ffd11d084f193a35327ed88d74a8
Ravi Arora <ravarora0@publicisgroupe.net>
2021-01-11 06:28:38 +0530

    Commit  python build in and custom modules changes

---
+++ main.py
@@ -0,0 +1,35 @@
+#built in module.
+import random
+import sys
+
+
+#Many way to import the package into main class.
+#Custom build module
+import pythonmodules.utilityhelper.utility
+#way 1
+from pythonmodules.utilityhelper.utility import multiply
+#way 2
+from pythonmodules.utilityhelper import utility
+#way 3
+from pythonmodules.shopping.shoppingcart import buy
+
+#if __name__ == "__main__":
+    # print(pythonmodules.utilityhelper.utility.addition(10,20))
+    # print(multiply(10, 10))
+    # print(utility.divide(20, 10))
+    # print(pythonmodules.shopping.shoppingcart.buy('Orange'))
+    # print(buy('Apple'))
+
+##print(dir(random))
+# print(random.random())
+# print(random.randint(1, 10))
+# print(random.choices([1, 2, 3, 4, 5]))
+# mylist = [1, 2, 3, 4]
+# random.shuffle(mylist)
+# print(mylist)
+
+#System module
+
+#print(dir(sys))
+
+#print(sys.argv)
