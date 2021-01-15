557a7464e47a9de2bdd28c8ec22b3e9f637d9682
Ravi Arora <ravarora0@publicisgroupe.net>
2021-01-15 13:24:35 +0530

    Custom & Builtin Modules changes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #built in module.
 from random import random
 import sys
+import pyjokes
 
+#collection module
+from collections import Counter,OrderedDict,defaultdict
 
 #Many way to import the package into main class.
 #Custom build module
@@ -23,7 +26,38 @@ if __name__ == "__main__":
     # print(buy('Apple'))
 
     #Guess the number 
-    game()
+    # game()
+    
+    #print the pyjokes
+   # joke=pyjokes.get_joke('en','neutral')
+    #print(f"Pyjoke is : ",joke)
+
+
+    #collection module
+
+    list = [1, 2, 1, 3, 4, 5, 6, 4, 6, 7]
+    
+    print(Counter(list))
+
+    #default dict
+
+    dict = defaultdict(int, {'a': 1, 'b': 2})
+    
+    dict1 = defaultdict(lambda:'Does not exists',{'a': 1, 'b': 2})
+    print(dict['c'])
+    print(dict1['c'])
+
+    #OrderDict
+
+    dict3 = OrderedDict({'c': 1})
+    dict3['a'] = 2
+    dict3['b']=3
+
+    dict4 = OrderedDict({'c': 1})
+    dict4['b'] = 3
+    dict4['a']=2
+    
+    print(dict3==dict4)
 
 ##print(dir(random))
 # print(random.random())
@@ -38,6 +72,7 @@ if __name__ == "__main__":
 #print(dir(sys))
 
 #print(sys.argv)
+
 
 
 
