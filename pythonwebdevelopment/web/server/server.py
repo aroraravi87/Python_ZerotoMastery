04242dfe77edecde37e12ac07683a5d28e6eec08
Ravi Arora <ravarora0@publicisgroupe.net>
2021-01-25 09:25:59 +0530

    Commit changes to create new website

--- pythonwebdevelopment/web/server/server.py
+++ pythonwebdevelopment/web/server/server.py
@@ -7,20 +7,6 @@ print(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route('/<username>')
-def index(username=None):
-    if username=="home":
-        return render_template("index.html")
-    elif username == "about":
-        return render_template("about.html")
-    elif username == "news":
-        return render_template("news.html")
-
-# @app.route('/about')
-# def about():
-#      return render_template("about.html")
-
-
-# @app.route('/news')
-# def news():
-#     return render_template("news.html")
+@app.route('/<string:page_name>')
+def index(page_name=None):
+    return render_template(page_name)
